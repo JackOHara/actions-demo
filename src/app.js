80e1e2dfbc96797fe0060d088391592ca6998315
Stephen McAuley <steviebiddles@gmail.com>
2020-07-10 15:21:11 +0100

    chore: add release workflows

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send({
-        message: `Hello, ${req.query.name || 'World'}!`
-    });
-})
+  res.send({
+    message: `Hello, ${req.query.name || 'World'}!`,
+  });
+});
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
